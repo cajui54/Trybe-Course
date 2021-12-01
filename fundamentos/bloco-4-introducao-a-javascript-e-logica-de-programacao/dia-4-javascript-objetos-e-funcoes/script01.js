c9937dc5be6aacb555a926ce0355b880d314b1e3
Jackson Silva <cajui54@gmail.com>
2021-12-01 15:44:20 -0300

    4/4 exercicios 1/11 resolvido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script01.js
@@ -0,0 +1,28 @@
+/* 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:*/
+
+function sum(number1, number2){
+    return number1 + number2;
+}
+
+function sub(number1, number2){
+    return number1 - number2;
+}
+function mult(number1, number2){
+    return number1 * number2;
+}
+function mult(number1, number2){
+    return number1 / number2;
+}
+function mod(number1, number2){
+    return number1 % number2;
+}
+
+
+console.log('Somar: ' + sum(11,20));
+console.log('Subtração: ' + sub(11,20));
+console.log('Multiplicação: ' + mult(11,20));
+console.log('Divisão: ' + mult(11,20));
+console.log('Módulo: ' + mod(3,2));
+
+
+
