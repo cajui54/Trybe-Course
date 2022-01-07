b0c71be888eb9070de95c9f3af1a40887214bdec
Jackson Silva <cajui54@gmail.com>
2022-01-06 21:58:28 -0300

    resoluçao do exercicio 6.1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/js/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/js/script.js
@@ -25,50 +25,103 @@ let arrEstados = [
     'Rondônia',
     'Roraima',
     'Tocantis'
-]
-let $cmbEstados = document.getElementById('cmbEstados');
-let $inputDate = document.getElementById('input-date');
-let $button = document.querySelector('button')
+];
+const $selectState = document.querySelector('#select-state');
+let $inputDateStart = document.getElementById('input-date-start');
+let $btnSubmit = document.querySelector('#btnSubmit');
 let $form = document.querySelector('form');
-
-function loadComboxEstados(){
-    arrEstados.sort();
-    arrEstados.forEach(_element =>{
-        let option = document.createElement('option');
-        option.textContent = _element;
-        $cmbEstados.appendChild(option);
-    });
-    
-}
-loadComboxEstados();
-
-$inputDate.addEventListener('keypress',()=>{
-    let dateValue = $inputDate.value;
-    if(dateValue.length == 2 || dateValue.length == 5){
-        $inputDate.value += "/";
-    }
-});
-
-$button.addEventListener('click', (event)=>{
+$btnSubmit.addEventListener('click',(event)=>{
     event.preventDefault();
-    let day = parseInt($inputDate.value.substr(0,2));
-    let month = parseFloat($inputDate.value.substr(3,4));
-    let year =  parseFloat($inputDate.value.substr(6,9));
-    console.log(day, month, year);
-    let msg =""
-    if(day > 0 || day <=31){
-        msg += "dia incorreto \n";
-    }
-    if(month > 0 || month <=12){
-        msg += "mês incorreto \n";
-    }
-    if(year < 0){
-        msg += 'ano incorreto \n';
-    }
-    if(msg != ''){
-        alert(msg);
+    let dataInput = getInputs();
+    
+    if(validationField(dataInput)){
+        outputValue(dataInput);
     }
 });
-$form.addEventListener('submit', (event)=>{
+
+function getInputs(){
+    let dataUser = {};
+    dataUser.name = document.getElementById('input-name');
+    dataUser.email = document.getElementById('input-email');
+    dataUser.cpf = document.getElementById('input-cpf');
+    dataUser.adress = document.getElementById('input-adress');
+    dataUser.state = document.querySelector('#select-state');
+    dataUser.dateStart = document.querySelector('#datepicker');
+
+    return dataUser;
+}
+function outputValue(_data){
+    let $outputDiv = document.getElementById('output');
+    $outputDiv.innerText = `
+                Nome: ${_data.name.value}
+                Email: ${_data.email.value}
+                CPF: ${_data.cpf.value}
+                Endereço: ${_data.adress.value}
+                Estado: ${_data.state.value}
+                Data Início: ${_data.dateStart.value}
+                `;
+
+                    
+}
+
+function validationField(_data){
+    if(_data.cpf.value == "" || _data.cpf.value.length > 11){
+        alert('campo cpf não preenchido');
+        return false;
+    }
+    return true;
+}
+// function validationDate(){
+//     let day = parseInt($inputDateStart.value.substr(0,2));
+//     let month = parseInt($inputDateStart.value.substr(3,2));
+//     let year = parseInt($inputDateStart.value.substr(6,4));
+//     let msgError = "";
+//     if(day < 1 || day > 31){
+//         msgError += `ocorreu um erro, formato dia incorreto, você digitou ${day} !\n`
+//     }
+//     if(month <1 || month > 12){
+//         msgError += `ocorreu um erro, formato mês incorreto, você digitou ${month} !\n`
+//     }
+//     if(year < 1){
+//         msgError += `ocorreu um erro, formato ano incorreto, você digitou ${year} !\n`
+//     }
+//     if($inputDateStart.value == ""){
+//         msgError += `ocorreu um erro, você não preencheu o campo date de início! \n`
+//     }
+//     if(msgError != ""){
+//         alert(msgError);
+//         return false;
+//     }
+//     return true;
+// }
+
+$form.addEventListener('submit',(event)=>{
     event.preventDefault();
 })
+
+// $inputDateStart.addEventListener('keypress', ()=>{
+//     let date = $inputDateStart.value;
+//     if(date.length == 2 || date.length == 5){
+//         $inputDateStart.value += "/"
+//     }
+// });
+
+function loadStatesSelect(){
+    arrEstados.sort();
+    arrEstados.forEach(elemet =>{
+        let option = document.createElement('option');
+        option.textContent = elemet;
+        $selectState.appendChild(option);
+    });
+}
+loadStatesSelect();
+
+// var picker = new Pikaday({ field: document.getElementById('datepicker') });
+
+// var picker = new Pikaday({
+//     field: document.getElementById('datepicker'),
+//     format: 'D MMM YYYY',
+//     onSelect: function() {
+//         console.log(this.getMoment().format('Do MMMM YYYY'));
+//     }
+// });
